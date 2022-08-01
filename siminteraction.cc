53ffe36a8c3f0bac2818877b6310115218921985
danivenk <danivenk@gmail.com>
2022-08-01 18:11:49 +0200

    cherry-pick c415f11 from aburch/simutrans:master

--- siminteraction.cc
+++ siminteraction.cc
@@ -303,7 +303,7 @@ bool interaction_t::process_event( event_t &ev )
 
 	// Handle map drag with right-click
 
-	static bool left_drag = false;
+	static bool is_world_dragging = false;
 
 	if(IS_RIGHTCLICK(&ev)) {
 		display_show_pointer(false);
@@ -317,13 +317,13 @@ bool interaction_t::process_event( event_t &ev )
 		catch_dragging();
 		move_view(ev);
 	}
-	else if(  IS_LEFTDRAG(&ev)  &&  IS_LEFT_BUTTON_PRESSED(&ev)  &&  (left_drag  ||  !world->get_tool(world->get_active_player_nr())->move_has_effects())  ) {
+	else if(  IS_LEFTDRAG(&ev)  &&  IS_LEFT_BUTTON_PRESSED(&ev)  &&  (is_world_dragging  ||  !world->get_tool(world->get_active_player_nr())->move_has_effects())  ) {
 		/* ok, we have a general tool selected, and we have a left drag or left release event with an actual difference
 		 * => move the map, if we are beyond a threshold */
-		if(  left_drag  ||  abs(ev.cx-ev.mx)+abs(ev.cy-ev.my)>=env_t::scroll_threshold  ) {
-			if (!left_drag) {
+		if(  is_world_dragging  ||  abs(ev.cx-ev.mx)+abs(ev.cy-ev.my)>=env_t::scroll_threshold  ) {
+			if (!is_world_dragging) {
 				display_show_pointer(false);
-				left_drag = true;
+				is_world_dragging = true;
 			}
 			world->get_viewport()->set_follow_convoi(convoihandle_t());
 			catch_dragging();
@@ -332,11 +332,11 @@ bool interaction_t::process_event( event_t &ev )
 		}
 	}
 
-	if( !IS_LEFT_BUTTON_PRESSED(&ev)  &&  left_drag  ) {
+	if( !IS_LEFT_BUTTON_PRESSED(&ev)  &&  is_world_dragging  ) {
 		// show the mouse and swallow this event if we were dragging before
 		ev.ev_code = IGNORE_EVENT;
 		display_show_pointer(true);
-		left_drag = false;
+		is_world_dragging = false;
 	}
 
 
